3d3554658f28ae1d6b9b29c58753d37397697611
S1ngS1ng <liuxing0514@gmail.com>
2018-07-11 00:05:08 -0700

    (WIP) Separate logger to lib/reporter. generate-line-output.js not done

--- process/generate-status-output.js
+++ process/generate-status-output.js
@@ -1,16 +1,6 @@
 const nodegit = require('nodegit');
-const chalk = require('chalk');
-const log = console.log;
-const { getIgnore } = require('./git-helper/get-ignore');
-
-const wtLabel = {
-    WT_DELETED: chalk.bold.bgRed('Deleted'),
-    WT_MODIFIED: chalk.bold.bgGreen('Modified'),
-    WT_NEW: chalk.bold.bgYellowBright('New'),
-    WT_RENAMED: chalk.bold.bgCyan('Renamed'),
-    WT_TYPECHANGE: chalk.bold.bgBlue('Typechange'),
-    WT_UNREADABLE: chalk.inverse('Unreadable')
-};
+const { getIgnore } = require('../lib/git-helper/get-ignore');
+const { loggerStatus, loggerWarning } = require('../lib/reporter/logger-status');
 
 /**
  * @function generateStatusOutput
@@ -26,11 +16,11 @@ module.exports.generateStatusOutput = async (repoStatus, repo) => {
     let ignoredList = await getIgnore(repo.path());
     nodegit.Ignore.addRule(repo, ignoredList);
     nodegit.Ignore.addRule(repo, '.idea/');
-    log(chalk.underline.bgRedBright('Your curriculum-cn repo is not clean. Please either commit or stash your changes'));
+    loggerWarning('Your curriculum-cn repo is not clean. Please either commit or stash your changes');
 
     await repoStatus.forEach(async st => {
         if (await nodegit.Ignore.pathIsIgnored(repo, st.path()) === 0) {
-            log(wtLabel[st.status()], ` ${st.path()}`)
+            loggerStatus(st.status(), st.path());
         }
     });
 };
